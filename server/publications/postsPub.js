e26caccb1c61d5226012e09b118e8d895ccd80f7
Joseph Le <josephle@me.com>
2015-02-25 15:42:57 -0800

    Made posts return 2 posts by default and sorted by date

--- server/publications/postsPub.js
+++ server/publications/postsPub.js
@@ -1,3 +1,5 @@
-Meteor.publish('posts', function () {
-  return Posts.find();
+Meteor.publish('posts', function (limit) {
+  // http://stackoverflow.com/a/19165949
+  var lim = limit || 2;
+  return Posts.find({}, {limit: lim, sort: {createdAt: 1}});
 });
