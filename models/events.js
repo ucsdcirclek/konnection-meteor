7f22e7d48ac74148f3a2a490e292aa6806915b2b
Joseph Le <josephle@me.com>
2015-02-25 14:27:43 -0800

    Added events model

---
+++ models/events.js
@@ -0,0 +1,32 @@
+Events = new Mongo.Collection('events');
+
+Events.attachSchema(
+    new SimpleSchema({
+    title: {
+      type: String
+    },
+    content: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      denyUpdate: true
+    }
+  })
+);
+
+// Collection2 already does schema checking
+// Add custom permission rules if needed
+if (Meteor.isServer) {
+  Events.allow({
+    insert : function () {
+      return true;
+    },
+    update : function () {
+      return true;
+    },
+    remove : function () {
+      return true;
+    }
+  });
+}
