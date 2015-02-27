876d610f01181ddcb3970ed7f97af8855b6f5fe1
Joseph Le <josephle@me.com>
2015-02-26 16:03:05 -0800

    Added homepage

--- client/lib/spacebarHelpers.js
+++ client/lib/spacebarHelpers.js
@@ -27,6 +27,16 @@ Template.registerHelper('constant', function (what) {
 });
 
 // http://stackoverflow.com/a/22088138
-Template.registerHelper('formatDate', function(date) {
-  return moment(date).format('MMMM Do YYYY');
+Template.registerHelper('formatDate', function(date, format) {
+  if(arguments.length <= 1)
+    return moment(date).format('MMMM Do YYYY');
+  else
+    return moment(date).format(format);
+});
+
+Template.registerHelper('displayDay', function(daysAway) {
+  if(arguments.length == 0)
+    return moment().format('dddd');
+  else
+    return moment().add(daysAway, 'd').format('dddd');
 });
